7d2cad561a7052dfaf09b01c9129dd38bc39e82e
TMad <madsen.tyler@gmail.com>
2017-12-15 22:28:14 -0700

    build destination db, routes, controller, model, views

---
+++ db/migrate/20171216012146_create_destinations.rb
@@ -0,0 +1,11 @@
+class CreateDestinations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :destinations do |t|
+      t.string :name
+      t.string :city
+      t.string :state
+      t.decimal :latitude
+      t.decimal :longitude
+    end
+  end
+end
