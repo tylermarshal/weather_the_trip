8c3e5f6d00e3b8a9f33c879a9c686dbc0b9ed9ed
TMad <madsen.tyler@gmail.com>
2017-12-15 22:55:37 -0700

    begin building destination feature tests and factorybot

---
+++ spec/factories/destinations.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :destination do
+    sequence(:name) {|n| "Destination#{n}"}
+    sequence(:city) {|n| "City#{n}"}
+    sequence(:state) {|n| "State#{n}"}
+    sequence(:latitude) {|n| n}
+    sequence(:longitude) {|n| n}
+  end
+end
---
+++ spec/features/destinations/user_can_see_all_destinations.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe "When a user visits the destinations index page" do
+  it "A user sees the attributes of all destinations" do
+    
+  end
+end
