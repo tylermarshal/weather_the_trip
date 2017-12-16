7a91c9b40d7c6a5a991169eadaf6593e3446ee6e
TMad <madsen.tyler@gmail.com>
2017-12-15 23:09:26 -0700

    build seed file for destinations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+json = ActiveSupport::JSON.decode(File.read('db/seeds/cities.json'))
+
+json.each do |a|
+  Destination.create!(a['destination'], without_protection: true)
+end
