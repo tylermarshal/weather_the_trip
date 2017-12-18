a1b1ef18372f65097d6a8fb86c2cab0813856453
TMad <madsen.tyler@gmail.com>
2017-12-18 16:29:50 -0700

    build dynamic dest factorybot

--- spec/factories/destinations.rb
+++ spec/factories/destinations.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :destination do
-    city "San Francisco"
-    state "CA"
-    latitude 37.7749295
-    longitude (-122.4194155)
-    population 837442
-    # sequence(:city) {|n| "City#{n}"}
-    # sequence(:state) {|n| "State#{n}"}
-    # sequence(:latitude) {|n| n}
-    # sequence(:longitude) {|n| n}
-    # sequence(:population) {|n| n}
+    # city "San Francisco"
+    # state "CA"
+    # latitude 37.7749295
+    # longitude (-122.4194155)
+    # population 837442
+    sequence(:city) {|n| "City#{n}"}
+    sequence(:state) {|n| "State#{n}"}
+    sequence(:latitude) {|n| n}
+    sequence(:longitude) {|n| n}
+    sequence(:population) {|n| n}
   end
 end
