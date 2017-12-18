fdd7c8e6ee50981496f38aa0f70c66660cee7bb1
TMad <madsen.tyler@gmail.com>
2017-12-18 12:50:38 -0700

    add user routes, controller, model, view, links

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.notice = "Created #{@user.username}"
+      session[:user_id] = @user.id
+      
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root :to => 'home#index'
+
+  resources :users, only: [:show, :new, :create]
 
   resources :trips
   resources :destinations do
     resources :recommendations
   end
 
-    root :to => 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
