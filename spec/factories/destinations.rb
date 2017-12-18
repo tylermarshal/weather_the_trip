35c04d4d4c0e54f65485d08794ea74a89316a493
TMad <madsen.tyler@gmail.com>
2017-12-18 13:33:12 -0700

    fix destination factory and dest index test

--- spec/factories/destinations.rb
+++ spec/factories/destinations.rb
@@ -1,10 +1,14 @@
 FactoryBot.define do
   factory :destination do
-    name "San Francisco"
-    # sequence(:name) {|n| "Destination#{n}"}
+    city "San Francisco"
+    state "CA"
+    latitude 37.7749295
+    longitude (-122.4194155)
+    population 837442
     # sequence(:city) {|n| "City#{n}"}
     # sequence(:state) {|n| "State#{n}"}
     # sequence(:latitude) {|n| n}
     # sequence(:longitude) {|n| n}
+    # sequence(:population) {|n| n}
   end
 end
