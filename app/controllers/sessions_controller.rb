a9997808eb309960d2c5b7fb74db5ef983b2b3cb
TMad <madsen.tyler@gmail.com>
2017-12-18 14:04:22 -0700

    add login test and fix session controller & new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ def create
   @user = User.find_by(username: params[:username])
   if @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
-    redirect_to root
+    flash.notice = "Welcome, #{current_user.username}!"
+    redirect_to root_path
   else
     render :new
   end
