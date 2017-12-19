b1bab14c36bf0ccda4dd7b02f4290c7c657ca907
TMad <madsen.tyler@gmail.com>
2017-12-18 19:52:48 -0700

    add admin/dest routes and users/trips routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ Rails.application.routes.draw do
     resources :trips
   end
 
-  resources :destinations do
-    resources :recommendations
+  scope :admin, module: :admin, as: :admin, only: [:new, :create, :edit, :update, :destroy] do
+    resources :destinations
   end
+
+  resources :destinations, only: [:index, :show]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
