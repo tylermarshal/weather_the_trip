fd26ace7305144ad13a3e7cb720450ebce4834bf
TMad <madsen.tyler@gmail.com>
2017-12-19 20:00:34 -0700

    update Routes to Route

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,7 +1,7 @@
 class RoutesController < ApplicationController
 
   def create
-    @route = Routes.new(destination_id: params[:destination_id], trip_id: current_trip.id)
+    @route = Route.new(destination_id: params[:destination_id], trip_id: current_trip.id)
     if @route.save
       flash.notice = "Destination Added to Trip"
       redirect_back(fallback_location: destinations_path)
@@ -9,7 +9,7 @@ class RoutesController < ApplicationController
   end
 
   def destroy
-    route = Routes.find_by(destination_id: params[:destination_id], trip_id: current_trip.id)
+    route = Route.find_by(destination_id: params[:destination_id], trip_id: current_trip.id)
     route.destroy
     flash.notice = "Destination Deleted from Trip"
     redirect_back(fallback_location: destinations_path)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
   def show
+    if current_user.id == params[:id].to_i
+      render :show
+    else
+      render file: '/public/404'
+    end
   end
 
   def new
